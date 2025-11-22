2a501854824514426d292cb63852bf75ebbe7c55
Rion Miyazaki <jrion13@gmail.com>
2025-11-21 16:09:54 -0800

    updated settings

--- app/src/main/java/com/example/peacefull_dessert_mob_dev/SettingsFragment.java
+++ app/src/main/java/com/example/peacefull_dessert_mob_dev/SettingsFragment.java
@@ -11,20 +11,83 @@ import androidx.fragment.app.Fragment;
 
 
 public class SettingsFragment extends Fragment {
-
+    LinearLayout account, paymentMethod, language, notification, privacy, switchAccount, logOut;
+    Switch darkMode;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // instantize View view as inflater.inflate
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // initialize all variables through view id
+        account = view.findViewById(R.id.clickBoxAccount);
+        darkMode = view.findViewById(R.id.switchDarkMode);
+        paymentMethod = view.findViewById(R.id.clickBoxPayment);
+        language = view.findViewById(R.id.clickBoxLanguage);
+        notification = view.findViewById(R.id.clickBoxNotification);
+        privacy = view.findViewById(R.id.clickBoxPrivacy);
+        switchAccount = view.findViewById(R.id.clickBoxSwitchAccount);
+        logOut = view.findViewById(R.id.clickBoxLogout);
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Take to new account fragment displaying email and user name (maybe set profile picture?)
+            }
+        });
+
+        darkMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // If toggled switch set all page backgrounds to dark and text to cream or white
+            }
+        });
+
+        paymentMethod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Take to card fragment with several editviews to input card name, number, cvv, expirey date
+            }
+        });
+
+        language.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // send dummy pop up this will only be implemented after everything else is done
+            }
+        });
+
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // send to fragment with a bunch of dummy switches labeled allow notifications, allow banner, etc
+            }
+        });
+
+        privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // send pop up dummy
+            }
+        });
+
+        switchAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // send to switch account fragment that allows the user to reinput username and password
+                //send toast message after resigned in to new account
+            }
+        });
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // send toast message that user has been successfully logged out and send back to the log in page
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
     }
 
-    EditText etLastName, etUsernsme, etEmail, etPassword, etConfirm;
-    Button btnCreate;
-
-//    @Override
-//    protected void onCreate(Bundle b) {
-//        super.onCreate(b);
-//        setContentView(R.layout.activity_)
-//    }
 }
